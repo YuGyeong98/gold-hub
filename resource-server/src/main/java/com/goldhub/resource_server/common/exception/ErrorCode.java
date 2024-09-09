5046281a6dab1fd79098b6c3638839939e6eb4e6
YuGyeong98 <kuk3097@naver.com>
2024-09-10 00:39:19 +0900

    #7 [feat] 자원 서버 ErrorCode enum 구현

---
+++ resource-server/src/main/java/com/goldhub/resource_server/common/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.goldhub.resource_server.common.exception;
+
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    /**
+     * 500 - Internal Server Error
+     */
+    SERVER_ERROR(INTERNAL_SERVER_ERROR, "서버 에러가 발생했습니다.");
+
+    private final HttpStatus httpStatus;
+    private final String message;
+}
