4f56a2cc4b83f3ee304673fc078144111fdd1ca2
YuGyeong98 <kuk3097@naver.com>
2024-09-10 14:20:13 +0900

    #12 [feat] 사용자 로그인 service 구현

--- auth-server/src/main/java/com/goldhub/auth_server/user/service/UserService.java
+++ auth-server/src/main/java/com/goldhub/auth_server/user/service/UserService.java
@@ -1,8 +1,13 @@
 package com.goldhub.auth_server.user.service;
 
+import com.goldhub.auth_server.common.config.auth.JwtToken;
+import com.goldhub.auth_server.common.config.auth.JwtUtil;
 import com.goldhub.auth_server.user.domain.User;
+import com.goldhub.auth_server.user.exception.PasswordUnauthorizedException;
 import com.goldhub.auth_server.user.exception.UsernameConflictException;
+import com.goldhub.auth_server.user.exception.UsernameUnauthorizedException;
 import com.goldhub.auth_server.user.repository.UserRepository;
+import com.goldhub.auth_server.user.service.dto.LoginUserDto;
 import com.goldhub.auth_server.user.service.dto.RegisterUserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,14 +15,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
-    @Transactional
     public void register(RegisterUserDto dto) {
         verifyUsername(dto.username());
 
@@ -25,9 +30,31 @@ public class UserService {
         userRepository.save(user);
     }
 
+    public LoginUserDto login(LoginUserDto dto) {
+        User user = findUser(dto);
+        JwtToken token = jwtUtil.createToken(user.getId());
+
+        return LoginUserDto.of(user, token);
+    }
+
+    private User findUser(LoginUserDto dto) {
+        User user = userRepository
+            .findByUsername(dto.getUsername())
+            .orElseThrow(() -> UsernameUnauthorizedException.EXCEPTION);
+        verifyPassword(dto.getPassword(), user.getPassword());
+
+        return user;
+    }
+
     private void verifyUsername(String username) {
         if (userRepository.existsByUsername(username)) {
             throw UsernameConflictException.EXCEPTION;
+        }
+    }
+
+    private void verifyPassword(String rawPassword, String encodedPassword) {
+        if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
+            throw PasswordUnauthorizedException.EXCEPTION;
         }
     }
 }
