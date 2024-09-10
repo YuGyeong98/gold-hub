753f0dce0e78a871b13c9289d666d03cdaf9c1ac
YuGyeong98 <kuk3097@naver.com>
2024-09-10 14:18:49 +0900

    #12 [feat] jwt 생성, 검증 클래스 구현

---
+++ auth-server/src/main/java/com/goldhub/auth_server/common/config/auth/JwtUtil.java
@@ -0,0 +1,93 @@
+package com.goldhub.auth_server.common.config.auth;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SecurityException;
+import java.security.Key;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import javax.crypto.SecretKey;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class JwtUtil {
+
+    private final Key key;
+    private final long accessTokenExpTime;
+    private final long refreshTokenExpTime;
+
+    public JwtUtil(
+        @Value("${jwt.secret}") String secretKey,
+        @Value("${jwt.time.access}") long accessTokenExpTime,
+        @Value("${jwt.time.refresh}") long refreshTokenExpTime
+    ) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+        this.accessTokenExpTime = accessTokenExpTime;
+        this.refreshTokenExpTime = refreshTokenExpTime;
+    }
+
+    public JwtToken createToken(Long userId) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", userId);
+
+        Instant now = Instant.now();
+
+        String accessToken = Jwts.builder()
+            .subject(String.valueOf(userId))
+            .claims(claims)
+            .issuedAt(Date.from(now))
+            .expiration(Date.from(now.plusSeconds(accessTokenExpTime)))
+            .signWith(key)
+            .compact();
+
+        String refreshToken = Jwts.builder()
+            .expiration(Date.from(now.plusSeconds(refreshTokenExpTime)))
+            .signWith(key)
+            .compact();
+
+        return JwtToken.builder()
+            .grantType("Bearer")
+            .accessToken(accessToken)
+            .refreshToken(refreshToken)
+            .build();
+    }
+
+    public boolean verifyToken(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty", e);
+        }
+        return false;
+    }
+
+    public Long getUserId(String token) {
+        return parseClaims(token).get("id", Long.class);
+    }
+
+    public Claims parseClaims(String token) {
+        return Jwts.parser()
+            .verifyWith((SecretKey) key)
+            .build()
+            .parseSignedClaims(token)
+            .getPayload();
+    }
+}
