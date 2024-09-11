ff6fda7f4bdcf250b36dc1daae6c56b15bc388e3
YuGyeong98 <kuk3097@naver.com>
2024-09-11 22:30:59 +0900

    #20 [feat] 주문 상태 업데이트 service 구현

--- resource-server/src/main/java/com/goldhub/resource_server/order/service/OrderService.java
+++ resource-server/src/main/java/com/goldhub/resource_server/order/service/OrderService.java
@@ -4,6 +4,11 @@ import com.goldhub.resource_server.common.config.JwtClient;
 import com.goldhub.resource_server.common.exception.InvalidTokenUnauthorizedException;
 import com.goldhub.resource_server.order.controller.response.FindOrderResponse;
 import com.goldhub.resource_server.order.domain.Order;
+import com.goldhub.resource_server.order.domain.OrderStatus;
+import com.goldhub.resource_server.order.domain.OrderType;
+import com.goldhub.resource_server.order.exception.InvalidPurchaseOrderStatusException;
+import com.goldhub.resource_server.order.exception.InvalidSaleOrderStatusException;
+import com.goldhub.resource_server.order.exception.OrderNotFoundException;
 import com.goldhub.resource_server.order.repository.OrderRepository;
 import com.goldhub.resource_server.order.service.dto.CreateOrderDto;
 import com.goldhub.resource_server.product.domain.Product;
@@ -17,9 +22,11 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
@@ -51,6 +58,19 @@ public class OrderService {
         return orderRepository.findBySearchCondition(pageable, date, invoiceType);
     }
 
+    public void updateStatus(String status, Long orderId, String accessToken) {
+        UserInfo userInfo = jwtClient.getUserInfo(accessToken);
+        verifyToken(userInfo);
+
+        Order order = orderRepository.findById(orderId)
+            .orElseThrow(() -> OrderNotFoundException.EXCEPTION);
+        OrderStatus orderStatus = OrderStatus.valueOf(status);
+
+        verifyOrderStatus(order, orderStatus);
+
+        order.updateStatus(orderStatus);
+    }
+
     private void verifyToken(UserInfo userInfo) {
         if (!userInfo.getIsValid()) {
             throw InvalidTokenUnauthorizedException.EXCEPTION;
@@ -67,5 +87,19 @@ public class OrderService {
         ProductType productType = ProductType.getProductType(value);
         return productRepository.findByType(productType)
             .orElseThrow(() -> ProductNotFoundException.EXCEPTION);
+    }
+
+    private void verifyOrderStatus(Order order, OrderStatus status) {
+        if (order.getType() == OrderType.PURCHASE) {
+            if (status != OrderStatus.ORDERED && status != OrderStatus.DEPOSITED
+                && status != OrderStatus.SHIPPED) {
+                throw InvalidPurchaseOrderStatusException.EXCEPTION;
+            }
+        } else {
+            if (status != OrderStatus.ORDERED && status != OrderStatus.REMITTED
+                && status != OrderStatus.RECEIVED) {
+                throw InvalidSaleOrderStatusException.EXCEPTION;
+            }
+        }
     }
 }
